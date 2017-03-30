a1ddd635971945ba730b1d346cde90d089561337
Dieter Menne <dieter.menne@menne-biomed.de>
2017-03-30 15:12:05 +0200

    Typo corrected

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -5,7 +5,7 @@ ui <- shinyUI(bootstrapPage(
  img(src = 'moved.jpg', align = "left"),
  h1("This page has moved ..."),
  a(href = "https://apps.menne-biomed.de/gastempt",
-  HTML("You have your stomach emptied, click here:<br>https://apps.menne-biomed.de/gastempt")),
+  HTML("To have your stomach emptied, click here:<br>https://apps.menne-biomed.de/gastempt")),
  img(src = 'screenshot.png')
 
 ))
